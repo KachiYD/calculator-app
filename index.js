9b4eeed605f228b017ab16d534dd9338fc32d1a9
KachiYD <kachi.oragui@gmail.com>
2024-02-24 22:07:48 -0700

    Removed other unnecessary files

--- index.js
+++
@@ -1,8 +0,0 @@
-function greet(name){
-    console.log('My dog is named ', name)
-
-    var btn = document.getElementById('myButton');
-    btn.innerHTML = "Hello " + name + "!!!!";
-}
-
-console.log('js loaded');
